aa381a85babdf0050e356121a3e60a4ed3a0bf8c
Anam Aadel <rruholislam1998@gmail.com>
2023-12-22 18:49:24 +0600

    added .env file

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAa9M8E7biouFwoxiJVYepPaWBcOlr1Ips",
-    authDomain: "fir-authenticationprojec-880fd.firebaseapp.com",
-    projectId: "fir-authenticationprojec-880fd",
-    storageBucket: "fir-authenticationprojec-880fd.appspot.com",
-    messagingSenderId: "226606973961",
-    appId: "1:226606973961:web:8347b58b677c8d4c5da4d8"
-  };
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+};
 
 // Initialize Firebase
   export const app = initializeApp(firebaseConfig);
